b82eb2dc96759299e01c71f360a419ff821e36d4
Nadno <eunadno@gmail.com>
2022-11-10 21:46:50 -0300

    feat(bubble-sort): peform a bubble sort in a list

---
+++ src/__tests__/bubble-sort.test.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from "vitest";
+
+import { numbers, strings } from "../lists";
+import { bubbleSort } from "../bubble-sort";
+
+describe("Bubble sort algorithm", () => {
+  const ascendingSortComparator = (a, b) => a - b;
+
+  const sortedNumbers = [...numbers].sort(ascendingSortComparator),
+    sortedStrings = [...strings].sort();
+
+  const staticUnsortedNumbers = [3, 9, 8, 5, 6, 4, 10, 1, 7, 2],
+    staticSortedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  it("should sort items ascending", () => {
+    expect(bubbleSort([...numbers], "asc")).toEqual(sortedNumbers);
+    expect(bubbleSort([...strings], "asc")).toEqual(sortedStrings);
+
+    expect(bubbleSort([...staticUnsortedNumbers], "asc")).toEqual(
+      staticSortedNumbers
+    );
+  });
+
+  it("should sort items descending", () => {
+    expect(bubbleSort([...numbers], "desc")).toEqual(
+      [...sortedNumbers].reverse()
+    );
+
+    expect(bubbleSort([...strings], "desc")).toEqual(
+      [...sortedStrings].reverse()
+    );
+
+    expect(bubbleSort([...staticUnsortedNumbers], "desc")).toEqual(
+      [...staticSortedNumbers].reverse()
+    );
+  });
+
+  it("should sort a bunch of items in any order", () => {
+    const randomNumbers = Array.from({ length: 500 }, () =>
+        Math.floor(Math.random() * 500)
+      ),
+      sortedRandomNumbers = [...randomNumbers].sort(ascendingSortComparator);
+
+    expect(bubbleSort([...randomNumbers])).toEqual(sortedRandomNumbers);
+    expect(bubbleSort([...randomNumbers], "desc")).toEqual(
+      [...sortedRandomNumbers].reverse()
+    );
+  });
+});
---
+++ src/bubble-sort.ts
@@ -0,0 +1,31 @@
+export const bubbleSort = <T>(
+  list: T[],
+  order: "asc" | "desc" = "asc"
+): T[] => {
+  const listLength = list.length;
+  if (!listLength) return list;
+
+  const compare =
+    order === "asc"
+      ? (current: T, next: T) => next < current // ASCENDING
+      : (current: T, next: T) => next > current; // DESCENDING
+
+  for (let currentIndex = 0; currentIndex < listLength - 1; currentIndex++) {
+    let current = list[currentIndex];
+
+    for (
+      let nextIndex = currentIndex + 1;
+      nextIndex < listLength;
+      nextIndex++
+    ) {
+      const next = list[nextIndex];
+      if (compare(current, next)) {
+        list[currentIndex] = next;
+        list[nextIndex] = current;
+        current = next;
+      }
+    }
+  }
+
+  return list;
+};
